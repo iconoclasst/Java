64ba9a70be46649715f51e45477ab16eb5972450
iconoclasst <davi09bezerra70@gmail.com>
2023-11-23 19:40:01 -0300

    atv01

---
+++ atv01/Data.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Data {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        
+        int hour = scanner.nextInt();
+        int min = scanner.nextInt();
+        int day = scanner.nextInt();
+        int mm = scanner.nextInt();
+        int year = scanner.nextInt();
+
+        System.out.printf("%02d:%02d %02d/%02d/%02d", hour, min, day, mm, year);
+
+        scanner.close();
+    }
+}
